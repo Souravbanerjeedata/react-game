2d67986c77001499fe1b26927981dfd945f8aa6b
Sourav <sourav.banerjeejobs@gmail.com>
2025-09-27 15:08:18 +0530

    created remaining time to show score and win/lost

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,26 +1,30 @@
 import { useImperativeHandle, useRef } from "react";
 
-const ResultModal = ({ ref, result, targetTime }) => {
+const ResultModal = ({ ref, targetTime, remainingTime, onReset }) => {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const fomattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
   useImperativeHandle(ref, () => {
     return {
       open() {
-        dialog.current.showModal(); 
+        dialog.current.showModal();
       },
     };
   });
 
   return (
     <dialog ref={dialog} className="result-modal">
-      <h2>You {result}</h2>
+      {userLost && <h2>You Lost</h2>}
       <p>
         The target was <strong>{targetTime} seconds.</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{" "}
+        <strong>{fomattedRemainingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
